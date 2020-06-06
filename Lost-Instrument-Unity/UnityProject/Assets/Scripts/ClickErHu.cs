176a016b0e2b1e2b840d71ac8f4401313a61248d
stern <1536768420@qq.com>
2020-06-06 22:49:48 +0800

    commit message

---
+++ Lost-Instrument-Unity/UnityProject/Assets/Scripts/ClickErHu.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class ClickErHu : MonoBehaviour
+{
+    public bool isWatching = false;
+    private RaycastHit ObjHit;
+    private Ray CustomRay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (!isWatching && Input.GetMouseButtonDown(0))
+        {
+            #if UNITY_ANDROID || UNITY_IPHONE
+            //移动端判断如下
+            if (EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId))
+            #else
+            //PC端判断如下
+            if (EventSystem.current.IsPointerOverGameObject())
+            #endif
+            {
+                Debug.Log("当前点击在UI上" + EventSystem.current.currentSelectedGameObject);
+            }
+            else
+            {
+                //从摄像机发出一条射线,到点击的坐标
+                CustomRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+                //显示一条射线，只有在scene视图中才能看到
+                Debug.DrawLine(CustomRay.origin, ObjHit.point, Color.red, 2);
+                if (Physics.Raycast(CustomRay, out ObjHit, 100))
+                {
+                    if (ObjHit.collider.gameObject != null)
+                    {
+                        Debug.Log("Click Object:" + ObjHit.collider.gameObject.name);
+                        if("ErHuBox" == ObjHit.collider.gameObject.name)
+                        {
+                            Debug.Log("Jump!");
+                            //SceneManager.LoadScene("ErHuScene");
+
+                            // 访问单个模型
+                            WatchErHu();
+                        }
+                    }
+                }
+                else
+                {
+                    Debug.Log("Click Null");
+                }
+            }
+        }
+    }
+
+    //void onClick() {
+    //    SceneManager.LoadScene("ErHuScene");
+    //}
+    public void ExitWatching()
+    {
+        isWatching = false;
+    }
+
+    public void WatchErHu()
+    {
+        isWatching = true;
+        Debug.Log("WatchErHu!");
+        //this.GetComponent<TouchMovement>().EnterWatching();
+        //Debug.Log("WatchErHu!!!");
+        GameObject.Find("Main Camera").GetComponent<TouchMovement>().EnterWatching();
+        
+    }
+}
---
+++ Lost-Instrument-Unity/UnityProject/Assets/Scripts/SelectRotate.cs
@@ -0,0 +1,143 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectRotate : MonoBehaviour
+{
+    public GameObject center;
+    public int position;
+    public float rotateSpeed;
+    public int direction = 0;  //0向右，1向左
+    public bool rotate;  //是否开始旋转
+    private float angle = 30.0f;
+    private float achieveAngle = 0f;
+    private float actualApeed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //rotate = false;
+        //if (direction == 0)
+        //{
+        //    rotateSpeed = -rotateSpeed;
+        //}
+        //if (position == 0 || position == 3)
+        //{
+        //    rotateSpeed *= 3.85f;
+        //}
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (direction == 0)
+        //{
+        //    rotateSpeed = -Mathf.Abs(rotateSpeed);
+        //}
+        //else
+        //{
+        //    rotateSpeed = Mathf.Abs(rotateSpeed);
+        //}
+
+        if (angle <= achieveAngle)
+        {
+            Debug.Log("angle" + position.ToString() + ":" + angle.ToString());
+            Debug.Log("achieveAngle" + position.ToString() + ":" + achieveAngle.ToString());
+
+            rotate = false;
+            GameObject.Find("Main Camera").SendMessage("stopRotate");
+
+            if (direction == 0)
+            {
+                position = (position + 1) % 4;
+                transform.RotateAround(center.transform.position, Vector3.up, achieveAngle - angle);
+            }
+            else
+            {
+                position = (position + 3) % 4;
+                transform.RotateAround(center.transform.position, Vector3.up, angle - achieveAngle);
+            }
+            angle = 30.0f;
+            achieveAngle = 0f;
+        }
+
+        if (rotate)
+        {
+            if (direction == 0)
+            {
+                switch (position)
+                {
+                    case 0:
+                        angle = 90.0f + Mathf.Asin(0.8f) * 180 / Mathf.PI;
+                        actualApeed = 3.85f * rotateSpeed;
+                        break;
+                    case 1:
+                        angle = Mathf.Asin(0.6f) * 180 / Mathf.PI;
+                        actualApeed = rotateSpeed;
+                        break;
+                    case 2:
+                        angle = Mathf.Asin(0.6f) * 180 / Mathf.PI;
+                        actualApeed = rotateSpeed;
+                        break;
+                    case 3:
+                        angle = 90.0f + Mathf.Asin(0.8f) * 180 / Mathf.PI;
+                        actualApeed = 3.85f * rotateSpeed;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else
+            {
+                switch (position)
+                {
+                    case 0:
+                        angle = 90.0f + Mathf.Asin(0.8f) * 180 / Mathf.PI;
+                        actualApeed = 3.85f * rotateSpeed;
+                        break;
+                    case 1:
+                        angle = 90.0f + Mathf.Asin(0.8f) * 180 / Mathf.PI;
+                        actualApeed = 3.85f * rotateSpeed;
+                        break;
+                    case 2:
+                        angle = Mathf.Asin(0.6f) * 180 / Mathf.PI;
+                        actualApeed = rotateSpeed;
+                        break;
+                    case 3:
+                        angle = Mathf.Asin(0.6f) * 180 / Mathf.PI;
+                        actualApeed = rotateSpeed;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            
+            achieveAngle += Mathf.Abs(actualApeed) * Time.deltaTime;
+            transform.RotateAround(center.transform.position, Vector3.up, actualApeed * Time.deltaTime);
+        }
+    }
+
+
+    public void RightButton()
+    {
+        if (!rotate)
+        {
+            rotate = true;
+            direction = 0;
+            rotateSpeed = -Mathf.Abs(rotateSpeed);
+        }
+    }
+
+    public void LeftButton()
+    {
+        if (!rotate)
+        {
+            rotate = true;
+            direction = 1;
+            rotateSpeed = Mathf.Abs(rotateSpeed);
+        }
+    }
+}
