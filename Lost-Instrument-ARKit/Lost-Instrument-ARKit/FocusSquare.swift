a7c499b00601931994554394d5fa4cfa11acf64d
齐旭晨 <1023578360@qq.com>
2020-06-13 22:10:38 +0800

    ARKit

---
+++ Lost-Instrument-ARKit/Lost-Instrument-ARKit/FocusSquare.swift
@@ -0,0 +1,40 @@
+//
+//  FocusSquare.swift
+//  Lost-Instrument-ARKit
+//
+//  Created by 齐旭晨 on 2020/6/13.
+//  Copyright © 2020 齐旭晨. All rights reserved.
+//
+
+import SceneKit
+
+class FocusSquare: SCNNode{
+    
+    var isClosed: Bool = true{
+        didSet{
+            geometry?.firstMaterial?.diffuse.contents = self.isClosed ? UIImage(named: "FocusSquare/close") : UIImage(named: "FocusSquare/open")
+        }
+    }
+    
+    override init() {
+        super.init()
+        
+        let plane = SCNPlane(width: 0.1, height: 0.1)
+        plane.firstMaterial?.diffuse.contents = UIImage(named: "FocusSquare/close")
+        plane.firstMaterial?.isDoubleSided = true
+        
+        geometry = plane
+        eulerAngles.x = GLKMathDegreesToRadians(-90)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    
+    
+    
+    
+}
---
+++ Lost-Instrument-ARKit/Lost-Instrument-ARKit/ViewControllerObjectAddition.swift
@@ -0,0 +1,46 @@
+//
+//  ViewControllerObjectAddition.swift
+//  Lost-Instrument-ARKit
+//
+//  Created by 齐旭晨 on 2020/6/13.
+//  Copyright © 2020 齐旭晨. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+extension ViewController {
+    
+    fileprivate func getModel(named name: String) -> SCNNode?{
+        let scene = SCNScene(named: "art.scnassets/\(name)/\(name).scn")
+        guard let model = scene?.rootNode.childNode(withName: "SketchUp", recursively: false) else{return nil}
+        
+        model.name = name
+        
+        return model
+    }
+    
+    @IBAction func addObjectButtonTapped(_ sender: Any) {
+        print("Add Button clicked")
+        
+        guard focusSquare != nil else{return}
+        
+        let modelName = "pipa-zhengti"
+        guard let model = getModel(named: modelName) else{
+            print("Unable to load \(modelName)")
+            return
+        }
+        
+        let hitTest = sceneView.hitTest(screenCenter, types: .existingPlaneUsingExtent)
+        guard let worldTransformColum3 = hitTest.first?.worldTransform.columns.3 else{ return }
+        model.position = SCNVector3(worldTransformColum3.x, worldTransformColum3.y, worldTransformColum3.z)
+        
+        sceneView.scene.rootNode.addChildNode(model)
+        print("\(modelName) added successfully")
+        
+        modelsInTheScene.append(model)
+        print("Currently have \(modelsInTheScene.count) model(s) in the scene" )
+    }
+    
+}
